ea5d297021ccfb47add8c169b1706b6c6adc8061
Tom <tomtarpeydev@gmail.com>
2020-02-13 09:29:52 +0000

    added the product super class

--- category.py
+++ category.py
@@ -1,6 +1,7 @@
 class Category:
-    def __init__(self, name): #TODO: , products)
+    def __init__(self, name, products = None):
         self.name = name
+        self.products = products
 
     def __str__(self):
         return f'No products available in {self.name}'
